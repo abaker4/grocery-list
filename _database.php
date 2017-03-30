a52f1f4d3e5966fd321bc2564326f9f9717a5501
Tim <tdlayer3@gmail.com>
2017-03-30 17:39:29 -0400

    add in boilerplates for endpoints and database connection

--- _database.php
+++ _database.php
@@ -1,1 +1,12 @@
+<?php
  // database connection
+
+$config = [
+    'user'      => 'root',
+    'pass'      => 'admin',
+    'database'  => 'grocerylist',
+    'host'      => 'db'
+];
+
+$db = new \PDO("mysql:host=" . $config['host'] . ";dbname=" . $config['database'], $config['user'], $config['pass']);
+
--- endpoint/item.php
+++ endpoint/item.php
@@ -1,1 +1,41 @@
-// item actions
+<?php
+
+require_once '../_database.php';
+
+
+// CREATE NEW ITEM
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+}
+
+// UPDATE EXISTING ITEM
+if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+
+}
+
+// DELETE ITEM
+if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+
+}
+
+// READ ITEMS
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $sql = "
+      SELECT * FROM item
+    ";
+
+    $stmt = $db->prepare($sql);
+    $stmt->execute(array());
+    $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+    return prepareResponse($result);
+}
+
+
+function prepareResponse($data) {
+    header('Content-Type: application/json"');
+    header('Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type, Accept');
+
+    return json_encode($data);
+}
