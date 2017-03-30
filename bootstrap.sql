b7d34d4540a3814fb6f5f2b6581fc5fbc927b1e8
Austin Baker <baker.austin.c@gmail.com>
2017-03-30 17:11:47 -0400

    add sql

---
+++ bootstrap.sql
@@ -0,0 +1,21 @@
+Create Database grocerylist;
+use grocerylist; 
+
+CREATE TABLE `list` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(355) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `item` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(355) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `fulfilled` tinyint(4) DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `list` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
